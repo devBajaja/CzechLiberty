30d25996eca25d466c4e5cca07706ea48d8d5178
devBajaja <martin.bajer@outlook.com>
2024-08-22 16:57:58 +0200

    Kompletně předělán navbar, funkční scrollování k názvu "STAFF TEAM" a zprovoznění náhodného generovácí čísel v index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-const randomNumber = Math.floor(Math.random() * 250) + 1;
-
-
-const randomNumberdiv = document.getElementById('randomNumberDisplay');
-
-randomNumberdiv.textContent = randomNumber + ' lidí';
-
-
 (function($){
     $(function(){
         
         /* Scroll to sections */
-        $(".jq--scroll-nabor").click(function(){
-            $("html, body").animate({scrollTop: $(".jq--nabor").offset().top}, 500);
-        });
- 
         $(".jq--scroll-staff-team").click(function(){
             $("html, body").animate({scrollTop: $(".jq--staff-team").offset().top}, 2000);
         });
     });
 })(jQuery);
+
+window.onload = function() {
+    const randomNumber = Math.floor(Math.random() * 250) + 1;
+    const randomNumberdiv = document.getElementById('randomNumberDisplay');
+
+    if (randomNumberdiv) {
+        randomNumberdiv.textContent = randomNumber + ' lidí';
+    } else {
+        console.error('Element with ID randomNumberDisplay was not found.');
+    }
+};
+
